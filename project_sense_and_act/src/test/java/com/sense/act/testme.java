252da27579d58946ccb8159eeeedf6c2caedba43
admin <vroom.cuteboy@gmail.com>
2016-09-04 16:47:22 +0530

    Surefire plugin included for re-run failed tests

---
+++ project_sense_and_act/src/test/java/com/sense/act/testme.java
@@ -0,0 +1,15 @@
+package com.sense.act;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testme {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+//mvn -Dsurefire.rerunFailingTestsCount=2 test
+	//mvn site will generate documentation under target > site
+}
